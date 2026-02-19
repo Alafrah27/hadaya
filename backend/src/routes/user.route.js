fc66a9fc6d3681d6e3fda42e70716b380af08262
[ali edris] <[almusdarthafa999@gmail.com]>
2026-02-19 13:53:05 -0800

    test email

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -7,6 +7,7 @@ import {
   getMe,
   resendOtp,
 } from "../controller/user.controller.js";
+import { testEmail } from "../controller/test.controller.js";
 import { JwtAuth } from "../middleware/jwt.js";
 
 const router = express.Router();
@@ -14,6 +15,7 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/test-email", testEmail);
 router.post("/verify-otp", JwtAuth, verifyEmail);
 router.get("/me", JwtAuth, getMe);
 router.post("/resend-otp", JwtAuth, resendOtp);
